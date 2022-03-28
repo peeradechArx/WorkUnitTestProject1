f4927fb5eda1f34aff890c06c2303533ab92d9b0
peeradechArx <96216541+peeradechArx@users.noreply.github.com>
2022-03-28 19:29:02 +0700

    Add files via upload

---
+++ WorkUnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WorkUnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WorkUnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("de137f9e-e94f-49b7-88bd-450b38593783")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WorkUnitTestProject/UnitTest2.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClassLibrary;
+
+using System;
+
+namespace WorkUnitTestProject
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void  Test_Assignment_1()
+        {
+            //ข้อ 1 
+            AssignmentClass ac = new AssignmentClass();
+            int input = 1020;
+            string result = ac.GradeCalculate(input);
+            Assert.AreEqual("A", result);
+            result = ac.GradeCalculate(80);
+            Assert.AreEqual("A", result);
+            result = ac.GradeCalculate(-1);
+            Assert.AreEqual("F", result);
+            result = ac.GradeCalculate(79);
+            Assert.AreEqual("B+", result);
+            result = ac.GradeCalculate(49);
+            Assert.AreEqual("F", result);
+
+        }
+        [TestMethod]
+        public void Test_Assignment_2()
+        {
+            //ข้อ 2 
+            AssignmentClass ac = new AssignmentClass();
+            double result = ac.GetDistance(3, 2, 7, 8);
+            Assert.AreEqual(7.21, result);
+            result = ac.GetDistance(-3, 2, 7, -8);
+            Assert.AreEqual(14.14, result);
+            result = ac.GetDistance(-3, -2, -7, -8);
+            Assert.AreEqual(7.21, result);
+            result = ac.GetDistance(3, -2, -7, 8);
+            Assert.AreEqual(14.14, result);
+        }
+        [TestMethod]
+        public void Test_Assignment_3()
+        {
+            //ข้อ 3 
+            AssignmentClass ac = new AssignmentClass();
+            int n = ac.GetFactorial(5);
+            Assert.AreEqual(120, n);
+            n = ac.GetFactorial(1);
+            Assert.AreEqual(1, n);
+            n = ac.GetFactorial(0);
+            Assert.AreEqual(1, n);
+            n = ac.GetFactorial(-1);
+            Assert.AreEqual(1, n);
+        }
+        [TestMethod]
+        public void Test_Assignment_4()
+        {
+            //ข้อ 4
+            AssignmentClass ac = new AssignmentClass();
+            string password = ac.CheackPassword("abc12345");
+            Assert.AreEqual("True", password);
+            password = ac.CheackPassword("abc123");
+            Assert.AreEqual(" ", password);
+            password = ac.CheackPassword("abcdefgh");
+            Assert.AreEqual(" ", password);
+            password = ac.CheackPassword("12345678");
+            Assert.AreEqual(" ", password);
+        }
+    }
+}
